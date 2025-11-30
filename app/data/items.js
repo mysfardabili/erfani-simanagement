e5ffd4cb437989cfc3ab6d5fb03b2bceae20f104
mysfnearby <2300016156@webmail.uad.ac.id>
2025-12-01 00:37:05 +0700

    perbaiki data

--- app/data/items.js
+++ app/data/items.js
@@ -4,13 +4,9 @@ export const wardrobeData = [
   { id: 't2', name: 'Kaos Hitam Oversize', category: 'atasan', src: 'https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?q=80&w=400&auto=format&fit=crop' },
   { id: 't3', name: 'Blazer Cream', category: 'atasan', src: 'https://images.unsplash.com/photo-1591047139829-d91aecb6caea?q=80&w=400&auto=format&fit=crop' },
   { id: 't4', name: 'Sweater Rajut', category: 'atasan', src: 'https://images.unsplash.com/photo-1576566588028-4147f3842f27?q=80&w=400&auto=format&fit=crop' },
-  { id: 't5', name: 'Jaket Denim', category: 'atasan', src: 'https://images.unsplash.com/photo-1523205565295-f8e91625443b?q=80&w=400&auto=format&fit=crop' },
-  { id: 't6', name: 'Tanktop Putih', category: 'atasan', src: 'https://images.unsplash.com/photo-1503342452485-86b7f54527ef?q=80&w=400&auto=format&fit=crop' },
   { id: 't8', name: 'Crop Top Pink', category: 'atasan', src: 'https://images.unsplash.com/photo-1618354691373-d851c5c3a990?q=80&w=400&auto=format&fit=crop' },
-  { id: 't9', name: 'Blouse Floral', category: 'atasan', src: 'https://images.unsplash.com/photo-1564257631407-4deb1f99d992?q=80&w=400&auto=format&fit=crop' },
   
   // === BAWAHAN (8 Item: Celana & Rok Saja) ===
-  { id: 'b2', name: 'Rok Plisket', category: 'bawahan', src: 'https://images.unsplash.com/photo-1583496661160-fb5886a0aaaa?q=80&w=400&auto=format&fit=crop' },
   { id: 'b3', name: 'Celana Bahan Hitam', category: 'bawahan', src: 'https://images.unsplash.com/photo-1594633312681-425c7b97ccd1?q=80&w=400&auto=format&fit=crop' },
   { id: 'b5', name: 'Kulot Putih', category: 'bawahan', src: 'https://images.unsplash.com/photo-1541099649105-f69ad21f3246?q=80&w=400&auto=format&fit=crop' },
   { id: 'b6', name: 'Celana Cargo', category: 'bawahan', src: 'https://images.unsplash.com/photo-1624378439575-d8705ad7ae80?q=80&w=400&auto=format&fit=crop' },
